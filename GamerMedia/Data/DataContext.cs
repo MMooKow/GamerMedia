7c3846e73de97d424c0c4cb69faf86192d4894ea
Dylan Restivo <dylan.restivo@yahoo.com>
2022-01-17 13:57:22 -0600

    Added data layer and dependencies

---
+++ GamerMedia/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GamerMedia.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
